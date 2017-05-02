6557ad88b3f28a2d83f438ed1cfcd9f1e9b9d320
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-05-02 12:59:02 +1200

    This playground reverses a String using the reversed() method

--- ReverseAString.playground/Contents.swift
+++ ReverseAString.playground/Contents.swift
@@ -1,5 +1,14 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+//This playground shows how to reverse a String
 
-var str = "Hello, playground"
+//This function accepts a String and by using  the reversed() method on its characters returns an array of characters in reverse order which is then converted into String and returned by the function
+func reverseAString(phrase: String) -> String
+{
+    return String(phrase.characters.reversed())
+}
+
+//Function calls
+
+//The reverseA String() is a called with  "I am a sentence." string as an argument and is assigned to the constant called output and printed to the console.
+let output = reverseAString(phrase: "I am a sentence.")
+print(output)
